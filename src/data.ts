e61f385e46dd0d3f9f4ea5a639bfba0dc1751fa0
Forest Park <foresthpark@gmail.com>
2023-07-20 15:01:27 -0600

    Refactor data.ts to correct a syntax error by removing an extra comma in the tech array

--- src/data.ts
+++ src/data.ts
@@ -56,7 +56,7 @@ export const data = {
         "Authored comprehensive documentation, both for internal team use and external clients, ensuring clear and concise instructions for seamless adoption and integration",
         "Collaborated closely with stakeholders, actively engaging in direct communication to gather and understand feature requirements, ensuring accurate implementation and alignment with business objectives",
       ],
-      tech: ["NextJs", "React", "Tailwind", "Antd", , "Golang"],
+      tech: ["NextJs", "React", "Tailwind", "Antd", "Golang"],
     },
     {
       id: "5",
